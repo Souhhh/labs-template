e5eeb4c7145239a97307a55c7950cec62c47d835
unknown <ins.guiza@gmail.com>
2019-04-15 15:13:48 +0200

    création du template ok, refractoring de la page home+ changement des liens dur grâce au get_template_directory_uri

---
+++ themes/labs-template/footer.php
@@ -0,0 +1,20 @@
+
+  <!-- Footer section -->
+  <footer class="footer-section">
+    <h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
+  </footer>
+  <!-- Footer section end -->
+
+
+
+
+  <!--====== Javascripts & Jquery ======-->
+  <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.1.4.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/magnific-popup.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/circle-progress.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+</body>
+
+</html>
